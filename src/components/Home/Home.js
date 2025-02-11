553802360446193ba22f5a520f9d069c1b4a51a9
Ashay Dharmik <ashay@theretailinsights.com>
2025-02-11 17:26:17 +0530

    added docker files

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -32,7 +32,7 @@ const Home = () => {
         </div>
         <div className="content_one">
           <div className="heading">
-            <span className="hello">Hello_</span>
+            <span className="hello">Hello</span>
             <h1>
               I'm <span>Ashay Dharmik</span>
             </h1>
